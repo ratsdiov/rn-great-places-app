c08e3add3d9b2106e28511a44c2db2574a1ec944
Keith Davidson <kbdsp@hotmail.com>
2022-01-01 22:16:26 -0500

    Start 237, started Add Place form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = () => {
+    const [titleValue, setTitleValue] = useState('');
+
+    const titleChangeHandler = text => {
+        // Todo, add validation
+        setTitleValue(text);
+    };
+
+    const savePlaceHandler = () => {
+        
+    }
     return (
-        <View>
-            <Text>New Place Screen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={titleChangeHandler}
+                    value={titleValue} />
+                <Button title="Save Place" color={Colors.primary} onPress={() => { }} />
+            </View>
+        </ScrollView>
     );
 };
 
 NewPlaceScreen.navigationOptions = {
     headerTitle: 'Add Place'
-}
+};
 
 export default NewPlaceScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    form: {
+        margin: 30,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15,
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2,
+    }
+});

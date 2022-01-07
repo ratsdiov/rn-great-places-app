d1e2aaee2ab988c1fee2c8a2816084e07d56cbe3
Keith Davidson <kbdsp@hotmail.com>
2022-01-06 22:21:10 -0500

    Start of 245, started sqlite setup

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,21 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = (title, image) => {
-    return { type: ADD_PLACE, placeData: { title: title, image: image} };
+    return async dispatch => {
+        const fileName = image.split('/').pop();
+        const newPath = FileSystem.documentDirectory + fileName;
+        try {
+            FileSystem.moveAsync({
+
+            });
+        } catch (err) {
+            console.log("file moe error", err);
+        }
+        dispatch({
+            type: ADD_PLACE,
+            placeData: { title: title, image: image }
+        });
+    };
 };

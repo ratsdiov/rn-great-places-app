39dcd11b6b44d72755c0e11e1fdcbb1bde94c599
Keith Davidson <kbdsp@hotmail.com>
2022-01-06 22:35:40 -0500

    Start of 246, added saving data to sqlite

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,6 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 
 export const ADD_PLACE = 'ADD_PLACE';
+import { insertPlace } from '../helpers/db';
 
 export const addPlace = (title, image) => {
     return async dispatch => {
@@ -8,14 +9,17 @@ export const addPlace = (title, image) => {
         const newPath = FileSystem.documentDirectory + fileName;
         try {
             FileSystem.moveAsync({
-
+                from: image,
+                to: newPath
+            });
+            const dbResult = await insertPlace(title, newPath, 'Dummy address', 15.6, 12.3);
+            console.log(dbResult);
+            dispatch({
+                type: ADD_PLACE,
+                placeData: { id: dbResult.insertId, title: title, image: image }
             });
         } catch (err) {
             console.log("file moe error", err);
         }
-        dispatch({
-            type: ADD_PLACE,
-            placeData: { title: title, image: image }
-        });
     };
 };
